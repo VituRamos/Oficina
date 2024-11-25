c4b41c7602a681bb9db943937e44d81e1f9c922a
Victor Ramos <107152739+VituRamos@users.noreply.github.com>
2024-11-25 18:13:01 -0300

    Add files via upload

---
+++ Mapa/NewTileSetShadows.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="NewTileSetShadows" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="TXShadow.png" width="512" height="512"/>
+</tileset>
